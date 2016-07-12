7dce91ddd80b4ec00cac9c445cfa242d9eba2a5a
CailleauThierry <thierrycailleau@yahoo.fr>
2016-07-12 14:33:20 -0700

    Displayinga cell by passing a position number

--- Excel_test.py
+++ Excel_test.py
@@ -3,7 +3,11 @@ import openpyxl as wb
 
 wb1 = wb.load_workbook('C:\Users\Administrator\Downloads\Example.xlsx')
 ws1 = wb1.get_sheet_by_name('Raw Data - MTD_5')
-res1 = ws1['A1'].value
-print(res1)
+res1 = ws1['O2'].value
+res2 = ws1.cell(row=2, column=15).value
 
-#this does display the value of Raw 1 Column A
+print(res1)
+print(res2)
+
+#this does display the value of Raw 2 Column O in 2 different fashion
+

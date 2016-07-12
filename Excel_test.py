8754e912a617c7860c4447632d8b1a63e62ee28c
CailleauThierry <thierrycailleau@yahoo.fr>
2016-07-12 14:37:09 -0700

    Displaying a cell even if it contains a long text string across multiple "lines" but the same raw

--- Excel_test.py
+++ Excel_test.py
@@ -3,11 +3,11 @@ import openpyxl as wb
 
 wb1 = wb.load_workbook('C:\Users\Administrator\Downloads\Example.xlsx')
 ws1 = wb1.get_sheet_by_name('Raw Data - MTD_5')
-res1 = ws1['O2'].value
-res2 = ws1.cell(row=2, column=15).value
+res1 = ws1['O3'].value
+res2 = ws1.cell(row=3, column=12).value
 
 print(res1)
 print(res2)
 
-#this does display the value of Raw 2 Column O in 2 different fashion
+#this does display the value of Raw 3 Column O and raw 3 Column L (i.e. column 12) even is the content of column 12 is a long text string
 
